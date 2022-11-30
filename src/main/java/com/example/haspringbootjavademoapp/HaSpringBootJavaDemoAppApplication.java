89a870af088f59346bfe7b23513e82d969316ee9
hasiabewardana <hasi.prasanga@gmail.com>
2022-12-01 01:32:37 +0530

    Started the Spring Boot project.

---
+++ src/main/java/com/example/haspringbootjavademoapp/HaSpringBootJavaDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.haspringbootjavademoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaSpringBootJavaDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaSpringBootJavaDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haspringbootjavademoapp/HaSpringBootJavaDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haspringbootjavademoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaSpringBootJavaDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
